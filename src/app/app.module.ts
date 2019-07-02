678649661e52f8e50aa6aea7fa7900e1c56eca96
mmjosedaniel <mmjosedaniel@hotmail.com>
2019-07-02 17:04:40 -0500

    Classes 25, 25

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,22 +16,9 @@ import { PlayerTableComponent } from './player-table/player-table.component';
 import { PlayerDialogComponent } from './player-dialog/player-dialog.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TeamTableComponent,
-    PlayerTableComponent,
-    PlayerDialogComponent
-  ],
-  imports: [
-    BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    AppRoutingModule
-  ],
-  providers: [
-    PlayerService,
-    TeamService
-  ],
+  declarations: [AppComponent, TeamTableComponent, PlayerTableComponent, PlayerDialogComponent],
+  imports: [BrowserModule, FormsModule, AngularFireModule.initializeApp(environment.firebase), AngularFireDatabaseModule, AppRoutingModule],
+  providers: [PlayerService, TeamService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/player-dialog/player-dialog.component.ts
+++ src/app/player-dialog/player-dialog.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Countries, SquadNumber } from '../interfaces/player';
 import { PlayerService } from '../services/player.service';
-import { take } from 'rxjs/operators';
 import { TeamService } from '../services/team.service';
+import { take } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-player-dialog',
@@ -10,11 +11,15 @@ import { TeamService } from '../services/team.service';
   styleUrls: ['./player-dialog.component.scss']
 })
 export class PlayerDialogComponent implements OnInit {
-  [x: string]: any;
   private team;
+  public player;
   public countries = Object.keys(Countries).map(key => ({ label: key, key: Countries[key] }));
-  public squadNumber = Object.keys(SquadNumber).slice(Object.keys(SquadNumber).length / 2);
-
+  public squadNumber = Object.keys(SquadNumber)
+    .slice(Object.keys(SquadNumber).length / 2)
+    .map(key => ({
+      label: key,
+      key: SquadNumber[key]
+    }));
   constructor(private playerService: PlayerService, private teamService: TeamService) {}
 
   ngOnInit() {
@@ -22,7 +27,7 @@ export class PlayerDialogComponent implements OnInit {
       .getTeams()
       .pipe(take(1))
       .subscribe(teams => {
-        if (teams.length < 0) {
+        if (teams.length > 0) {
           this.team = teams[0];
         }
       });
@@ -39,5 +44,14 @@ export class PlayerDialogComponent implements OnInit {
       players: [...(this.team.players ? this.team.players : []), playerFormValueKey]
     };
     this.teamService.editTeam(formattedTeam);
+  }
+
+  onSubmit(playerForm: NgForm) {
+    const playerFormValue = {...playerForm.value};
+    if (playerForm.valid) {
+      playerFormValue.leftFooted = playerFormValue.leftFooted === '' ? false : playerFormValue.leftFooted;
+    }
+    this.newPlayer(playerFormValue);
+    window.location.replace('#');
   }
 }
